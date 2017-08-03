5fc5fd9f0c746aa72b8188abded1ca947f87bdc0
Guillermo Sainz <yo@guillermosainz.com>
2017-08-03 12:32:59 -0400

    Adds initial camera fix and footer credits

--- tasks/upload_to_sketchfab.py
+++ tasks/upload_to_sketchfab.py
@@ -223,7 +223,10 @@ class UploadToSketchfab(Task):
                     'enable': True,
                     'exposure': 5.37483797816,
                     'rotation': 0,
-                    'uid': 'e73867d210de4bc2b5eb261738cf3e79'
+                    'uid': 'e73867d210de4bc2b5eb261738cf3e79',
+                    'shadowEnable': false,
+                    'shadowBias': 0.005,
+                    'lightIntensity': 1
                 }
             }}
 
